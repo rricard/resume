e15a367eca2acbb5804f41a7d4e1d5cff074cbde
Robin Ricard <ricard.robin@gmail.com>
2016-08-20 15:08:35 +0200

    Activate flow on all files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+/* @flow */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+// $FlowFixMe: Create in .flowconfig a mapper for !style
 import '!style!css!postcss!less!../node_modules/elemental/less/elemental.less';
 import './styles/index.css';
 
