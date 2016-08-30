a82ec9481efae0ffc0bd10ab803094c95c970d71
Robin Ricard <ricard.robin@gmail.com>
2016-08-31 00:19:25 +0200

    Move fonts so they are bundled with webpack

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,8 +1,6 @@
 rm -fr $(pwd)/build/data
-rm -fr $(pwd)/build/static/css/fonts
 rm -fr $(pwd)/build/images
 rm -f $(pwd)/build/rricard.pdf
 cp -R $(pwd)/data $(pwd)/build/data
-cp -R $(pwd)/fonts $(pwd)/build/static/css/fonts
 cp -R $(pwd)/images $(pwd)/build/images
 cp $(pwd)/rricard.pdf $(pwd)/build/rricard.pdf
